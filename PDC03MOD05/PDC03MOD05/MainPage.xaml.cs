40b6d26e34b7d920c78d264cacf31cee926fe7ad
mklnmrcd <152594846+mklnmrcd@users.noreply.github.com>
2024-05-30 13:10:03 +0800

    Add files via upload

---
+++ PDC03MOD05/PDC03MOD05/MainPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PDC03MOD05
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void OnDateSelected(object sender, DateChangedEventArgs args)
+        {
+            DateTime birthdate = birthDatePicker.Date;
+            DateTime today = DateTime.Today;
+            int age = today.Year - birthdate.Year;
+
+            result.Text = age.ToString();
+        }
+    }
+}
